c7d20259458093202421b52cbca3320b4d36708e
Sanyok004 <Sanyok004@yandex.ru>
2016-11-18 16:06:10 +0300

    Update

--- Lab5_App/views.py
+++ Lab5_App/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views import View
-
+from django.views.generic import View
 
 data = {'orders': []}
 for i in range(1, 10):
